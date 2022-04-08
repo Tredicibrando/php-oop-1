4e31f1ea02e5c458992dbf9ff51074da748ab268
Tredicibrando <nicolosigiov@gmail.com>
2022-04-08 12:59:23 +0200

    Finito

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+
+class movie{
+    
+    public $title;
+    public $subtitle;
+    public $vote;
+
+    function __construct(string $_title, string $_subtitle, float $_vote )
+    {
+        $this->title = $_title;
+        $this->subtitle = $_subtitle;
+        $this->vote = $_vote;
+    }
+
+    public function getFullTitle(){
+        return $this->title.' - '.$this->subtitle;
+    }
+
+
+}
+
+$LOTR_1 = new movie("Il signore degli anelli","la compagnia dell'anello",8.9);
+$LOTR_2 = new movie("Il signore degli anelli","Le due torri",8.8);
+$LOTR_3 = new movie("Il signore degli anelli","Il ritorno del re",9);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+<body>
+    
+
+        <ul>
+            <li>
+
+                <span>
+                <?php
+                    echo $LOTR_1->getFullTitle();
+                 ?>
+                </span>
+
+                <span>
+                <?php
+                    echo $LOTR_1->vote;
+                 ?>
+                </span>
+
+            </li>
+            <br>
+             <li>
+
+                <span>
+                <?php
+                    echo $LOTR_2->getFullTitle();
+                 ?>
+                </span>
+
+                <span>
+                <?php
+                    echo $LOTR_2->vote;
+                 ?>
+                </span>
+
+            </li>
+            <br>
+             <li>
+
+                <span>
+                <?php
+                    echo $LOTR_3->getFullTitle();
+                 ?>
+                </span>
+
+                <span>
+                <?php
+                    echo $LOTR_3->vote;
+                 ?>
+                </span>
+
+            </li>
+            <br>
+        </ul>
+
+
+</body>
+</html>
